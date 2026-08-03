de9893d1567d9f66b1ddc6c515ba772b7db82c84
unclewoozy <delacruzsigmund14@gmail.com>
2026-08-03 20:18:52 +0800

    Revert "add light dark theme toggle"
    This reverts commit c2bddf572ec8627e0260eba201cc04779aa8e876.

--- src/components/ide/TitleBar.jsx
+++ src/components/ide/TitleBar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import ThemeToggle from './ThemeToggle'
 
 export default function TitleBar() {
   const [time, setTime] = useState('')
@@ -33,7 +32,6 @@ export default function TitleBar() {
         </p>
 
         <div className="flex items-center gap-3">
-          <ThemeToggle />
           <p className="hidden font-mono text-[10px] uppercase tracking-[0.2em] text-fog sm:block">
             sigmund@portfolio.dev
           </p>
